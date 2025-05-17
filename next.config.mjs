44c3b65506e1adf7cf51f7f75f13836ca8bb03d9
Yash Vithalkar <92716787+Yashvithalkar@users.noreply.github.com>
2025-05-17 23:46:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning/" : "",
   output: "export",
   images: {
     unoptimized: true,
